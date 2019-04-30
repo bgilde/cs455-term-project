e1341ccf89b59ded5d7e363c0c57d15131a64779
Brandon <brandongildemaster@yahoo.com>
2019-04-29 21:29:40 -0600

    Update recognize.py

--- recognize.py
+++ recognize.py
@@ -5,93 +5,48 @@ from pyspark import SparkContext
 from pyspark.ml.image import ImageSchema
 import cv2
 import numpy as np
+from pyspark.ml.regression import LinearRegression
+from pyspark.ml.feature import VectorAssembler
 
 if __name__ == '__main__':
+
    sc = SparkContext(appName="Example App")
-   # sc.setMaster('yarn')
+
    image_rdd = ImageSchema.readImages("hdfs://pierre:41234/cs455/images_large").rdd
    image_rdd = image_rdd.repartition(15)
    image_rdd.cache()
-   # image_df.show()
-   # image_df.columns
-
 
    '''
          note: look into broadcast variables for the face_cascade and embedder variable to avoid reading them from disk each iteration
    '''
 
    ## this function will create a 128 feature embedding for each face, now we need to train a neural network on top of this
-   # def f(x, cascade, embed):
-   #    # face_cascade = cv2.CascadeClassifier('/s/chopin/a/grad/bgilde/distributed-systems/spark/python/haarcascade_frontalface_alt.xml')
-   #    # embedder = cv2.dnn.readNetFromTorch("/s/chopin/a/grad/bgilde/distributed-systems/spark/python/openface.nn4.small2.v1.t7")
-   #    face_cascade = cascade
-   #    embedder = embed
-   #    # print(face_cascade_broadcast)
-   #    # embedder = embedder_broadcast.value
-   #    # print("EMBEDDER")
-   #    # print(embedder)
-   #    filename = x.image.origin.split('/')[-1]
-   #    image = np.array(x.image.data)
-   #    image = image.reshape((480,854,3))
-   #    cv2.imwrite("/s/chopin/a/grad/bgilde/distributed-systems/spark/python/new_image.jpg", image)
-   #    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-   #    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-   #    for (x,y,w,h) in faces:
-   #       face = image[y:y+h,x:x+w,:]
-   #       cv2.imwrite("/s/chopin/a/grad/bgilde/distributed-systems/spark/python/test.jpg", face)
-   #       faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255,
-   #                (96, 96), (0, 0, 0), swapRB=True, crop=False)
-
-   #       embedder.setInput(faceBlob)
-   #       vec = embedder.forward()
-
-   #       return (filename, vec)
-         
-   # image_rdd = image_df.rdd
-
-   # collected = image_rdd.collect()
-
-   featuresAndLabels = []
-
-   # face_cascade = cv2.CascadeClassifier('/s/chopin/a/grad/bgilde/distributed-systems/spark/python/haarcascade_frontalface_alt.xml')
-   # embedder = cv2.dnn.readNetFromTorch("/s/chopin/a/grad/bgilde/distributed-systems/spark/python/openface.nn4.small2.v1.t7")
-
-   # for x in collected:
-   #    featuresAndLabels.append(f(x))
-
-   test = [x for x in image_rdd.toLocalIterator()]
-
-   face_cascade = cv2.CascadeClassifier('/s/chopin/a/grad/bgilde/distributed-systems/spark/python/haarcascade_frontalface_alt.xml')
-   embedder = cv2.dnn.readNetFromTorch("/s/chopin/a/grad/bgilde/distributed-systems/spark/python/openface.nn4.small2.v1.t7")
-
-   for x in test:
+   def f(x):
+      face_cascade = cv2.CascadeClassifier('/s/chopin/a/grad/bgilde/distributed-systems/spark/python/haarcascade_frontalface_alt.xml')
+      embedder = cv2.dnn.readNetFromTorch("/s/chopin/a/grad/bgilde/distributed-systems/spark/python/openface.nn4.small2.v1.t7")
       filename = x.image.origin.split('/')[-1]
       image = np.array(x.image.data)
       image = image.reshape((480,854,3))
-      cv2.imwrite("/s/chopin/a/grad/bgilde/distributed-systems/spark/python/new_image.jpg", image)
+      # cv2.imwrite("/s/chopin/a/grad/bgilde/distributed-systems/spark/python/new_image.jpg", image)
       gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
       faces = face_cascade.detectMultiScale(gray, 1.3, 5)
       for (x,y,w,h) in faces:
          face = image[y:y+h,x:x+w,:]
-         cv2.imwrite("/s/chopin/a/grad/bgilde/distributed-systems/spark/python/test.jpg", face)
+         # cv2.imwrite("/s/chopin/a/grad/bgilde/distributed-systems/spark/python/test.jpg", face)
          faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255,
                   (96, 96), (0, 0, 0), swapRB=True, crop=False)
 
          embedder.setInput(faceBlob)
          vec = embedder.forward()
 
-         featuresAndLabels.append((filename, vec))
+         return (filename.strip('0123456789.png'), vec)
 
-      # featuresAndLabels.append(f(x, cascade, embed))
+   # extract face embeddings
+   feature_rdd = image_rdd.map(lambda x: f(x))
 
-   print(featuresAndLabels)
+   # filter None values, meaning there was no face in the image
+   feature_rdd = feature_rdd.filter(lambda x: x is not None)
 
-   # features = image_rdd.map(lambda x: f(x))
+   # test = feature_rdd.collect()
 
-   # count = features.count()
-
-   # print("count: " + str(count))
-
-   # embedder = embedder_broadcast.value
-   # print("EMBEDDER")
-   # print(embedder)
+   print(feature_rdd.count())
